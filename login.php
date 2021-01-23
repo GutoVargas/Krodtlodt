9a9bbe6edf41c4d8a115d1c9b66febee91ea5866
GutoVargas <75427063+GutoVargas@users.noreply.github.com>
2021-01-23 20:38:43 -0200

    Criação do Projeto

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Vah Açaí - Login</title>
+        <title>Krodtlott - Login</title>
         <link href="css/krodtlott.css" rel="stylesheet">
         <script src="js/krodtloot.js"></script>
         
